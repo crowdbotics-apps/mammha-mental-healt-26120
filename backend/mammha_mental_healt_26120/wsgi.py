9472a11a402b59bcbb2edc51741018c7c33852ec
Crowdbotics <team@crowdbotics.com>
2021-05-04 20:13:36 +0000

    Update models

--- backend/mammha_mental_healt_26120/wsgi.py
+++ backend/mammha_mental_healt_26120/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mammha_mental_healt_26120.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mammha_mental_healt_26120.settings")
 
 application = get_wsgi_application()
